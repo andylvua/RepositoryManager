e93b0d36f0552cd8a4e8582adb09366ebd1e335d
Andrew Yaroshevych <ayaroshlvua@gmail.com>
2022-09-04 10:55:54 +0300

    Minor changes in notifications

--- static/scripts/utils/request_notification_permission.js
+++ static/scripts/utils/request_notification_permission.js
@@ -1,29 +1,35 @@
-(async () => {
+const notificationButton = $(".notification_button");
+
+(() => {
+    console.log("Checking notifications state");
+
+    if (localStorage.getItem('notifications') === "true") {
+        console.log("Notifications are enabled");
+        notificationButton.addClass("active");
+    } else {
+        console.log("Notifications are disabled");
+        notificationButton.removeClass("active");
+    }
+})();
+
+async function requestNotificationsPermission() {
     console.log("Awaiting notifications permission");
     let permission = await Notification.requestPermission();
     if (permission === "granted") {
         console.log("Notification permission granted.");
     }
-})();
-
-checkNotifications()
-
-function checkNotifications() {
-    console.log("Checking notifications state");
-
-    if (localStorage.getItem('notifications') === "true") {
-        $(".notification_button").addClass("active");
-    } else {
-        $(".notification_button").removeClass("active");
-    }
 }
 
-function toggleNotifications() {
+async function toggleNotifications() {
     if (localStorage.getItem('notifications') === "true") {
         localStorage.setItem('notifications', "false");
-        $(".notification_button").removeClass("active");
+        notificationButton.removeClass("active");
     } else {
+        notificationButton.addClass("pending");
+        await requestNotificationsPermission();
+        notificationButton.removeClass("pending");
+
         localStorage.setItem('notifications', "true");
-        $(".notification_button").addClass("active");
+        notificationButton.addClass("active");
     }
 }
